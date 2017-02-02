9383f73e8e776be18f28c431b29cd818eb8725de
Tarek Taha <tarek.taha@gmail.com>
2017-02-02 17:16:46 +0400

    - some useful info added

--- mavros_extras/scripts/swift_rtk_reroute.py
+++ mavros_extras/scripts/swift_rtk_reroute.py
@@ -8,6 +8,27 @@ from sbp.navigation import SBP_MSG_BASELINE_NED, MsgBaselineNED
 import argparse
 from sensor_msgs.msg import NavSatFix
 
+#/**
+# * @brief Pack a hil_gps message on a channel
+# * @param system_id ID of this system
+#* @param component_id ID of this component (e.g. 200 for IMU)
+# * @param chan The MAVLink channel this message will be sent over
+# * @param msg The MAVLink message to compress the data into
+# * @param time_usec Timestamp (microseconds since UNIX epoch or microseconds since system boot)
+# * @param fix_type 0-1: no fix, 2: 2D fix, 3: 3D fix. Some applications will not use the value of this field unless it is at least two, so always correctly fill in the fix.
+# * @param lat Latitude (WGS84), in degrees * 1E7
+# * @param lon Longitude (WGS84), in degrees * 1E7
+# * @param alt Altitude (AMSL, not WGS84), in meters * 1000 (positive for up)
+# * @param eph GPS HDOP horizontal dilution of position in cm (m*100). If unknown, set to: 65535
+# * @param epv GPS VDOP vertical dilution of position in cm (m*100). If unknown, set to: 65535
+# * @param vel GPS ground speed (m/s * 100). If unknown, set to: 65535
+# * @param vn GPS velocity in cm/s in NORTH direction in earth-fixed NED frame
+# * @param ve GPS velocity in cm/s in EAST direction in earth-fixed NED frame
+# * @param vd GPS velocity in cm/s in DOWN direction in earth-fixed NED frame
+# * @param cog Course over ground (NOT heading, but direction of movement) in degrees * 100, 0.0..359.99 degrees. If unknown, set to: 65535
+# * @param satellites_visible Number of satellites visible. If unknown, set to 255
+# * @return length of the message in bytes (excluding serial stream start sign)
+# */
 
 def gpsrtk():
     pub = rospy.Publisher('/mavros/gps_reroute/gps_fix', NavSatFix, queue_size=10)
@@ -28,13 +49,19 @@ def gpsrtk():
 		         # Print out representation of the message	
 			 	if msg.msg_type == 0x0201:    
 					msg1 = NavSatFix()   
-				  	print msg.lon
-					print msg.lat
-					print msg.height
+                                        print msg.lon # deg
+                                        print msg.lat # deg
+                                        print msg.height # meters
                                         msg1.header.stamp = rospy.Time.now()
 					msg1.latitude = msg.lon
 					msg1.longitude = msg.lat
 					msg1.altitude = msg.height
+                                        # Other fields from the message (according to the documentation page 12)
+                                        # msg.h_accuracy (mm)
+                                        # msg.v_accuracy (mm)
+                                        # msg.nsats
+                                        # msg.flags - check page 12 of documentation (basicaly: single point, differential, fixed rtk, f)
+                                        # msg.tow (time of week in ms)
 					pub.publish(msg1)
 	
       		except KeyboardInterrupt:
