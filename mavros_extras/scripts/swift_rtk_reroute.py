ed59e67f50f229d19e36f10e3f316652b9588d4d
reem90 <reem.ashour@kustar.ac.ae>
2017-02-05 15:28:44 +0400

    creating new msg for swift spp

--- mavros_extras/scripts/swift_rtk_reroute.py
+++ mavros_extras/scripts/swift_rtk_reroute.py
@@ -7,6 +7,7 @@ from sbp.client.loggers.json_logger import JSONLogger
 from sbp.navigation import SBP_MSG_BASELINE_NED, MsgBaselineNED
 import argparse
 from sensor_msgs.msg import NavSatFix
+from mavros_msgs.msg import SwiftSpp
 
 
 def gpsrtk():
@@ -27,16 +28,44 @@ def gpsrtk():
         		for msg, metadata in source.filter():
 		         # Print out representation of the message	
 			 	if msg.msg_type == 0x0201:    
+
+					msg1 = SwiftSpp()   
+				  	print msg.lon 						
+					print msg.lat
+					print msg.height
+					print msg.tow
+					print msg.h_accuracy
+					print msg.v_accuracy
+					print msg.n_sats
+                                        msg1.header.stamp = rospy.Time.now()
+					msg1.latitude_s = msg.lon * 10000000
+					msg1.longitude_s = msg.lat * 10000000
+					msg1.height_s = msg.height * 1000
+					msg1.tow_s = msg.tow
+					msg1.horizontal_acc_s = msg.h_accuracy
+					msg1.vertical_acc_s = msg.v_accuracy 
+					msg1.number_of_Sat = msg.n_sats 			
+					pub.publish(msg1)
+					'''
 					msg1 = NavSatFix()   
-				  	print msg.lon
+				  	print msg.lon 
 					print msg.lat
 					print msg.height
                                         msg1.header.stamp = rospy.Time.now()
-					msg1.latitude = msg.lon
-					msg1.longitude = msg.lat
-					msg1.altitude = msg.height
+					msg1.latitude = msg.lon * 10000000
+					msg1.longitude = msg.lat * 10000000
+					msg1.altitude = msg.height * 1000
 					pub.publish(msg1)
-	
+					'''
+				if msg.msg_type == 0x0202:    
+					print msg.x						
+					print msg.y
+					print msg.z
+					msg1.x_vector_from_base_to_rover = msg.x
+					msg1.y_vector_from_base_to_rover = msg.y
+					msg1.z_vector_from_base_to_rover = msg.z
+
+
       		except KeyboardInterrupt:
         		pass
     #rate = rospy.Rate(10) # 10hz
